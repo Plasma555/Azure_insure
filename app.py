56ad1e73d4e8893a66fbdccf7e8dfeeda1d68a88
Asouk <126856438+Plasma555@users.noreply.github.com>
2023-09-09 13:59:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+
+
+from flask import Flask, render_template, request, jsonify
+import requests,json
+
+app = Flask(__name__,template_folder=r"C:\Users\asouk\Desktop\Insurance_premium-main\templates")
+
+# Define the Azure ML real-time endpoint URL and API key
+AZURE_ML_ENDPOINT = "https://insurance-premium-ctzrj.eastus2.inference.ml.azure.com/score"
+API_KEY = "UROaOKBh5E2K4hlWc6zbGq7eTohRNqRz"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Retrieve form data
+        age = float(request.form["Age"])
+        bmi = float(request.form["Bmi"])
+        gender = request.form["Gender"]
+        region = request.form["region"]
+        smoker = request.form["smoker"]
+        children = float(request.form["Children"])
+
+        # Create the input data dictionary
+        data = {
+            "age": [age],
+            "sex": [gender],
+            "bmi": [bmi],
+            "region": [region],
+            "children": [children],
+            "smoker": [smoker]
+        }
+
+        # Set up the headers with the API key
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + API_KEY
+        }
+
+        # Make a POST request to the Azure ML real-time endpoint for predictions
+        response = requests.post(AZURE_ML_ENDPOINT, json=data, headers=headers)
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            prediction = data[0]
+            return render_template("index.html", prediction_text=f"Predicted Premium: ${prediction}")
+        else:
+            error_message = "Prediction request failed"
+            return render_template("index.html", error=error_message)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
